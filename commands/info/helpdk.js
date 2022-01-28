d53d7d4183055fb49c8f65aec2f56392f259e727
dark uu <71950796+darkdevgit@users.noreply.github.com>
2022-01-28 17:57:55 +0100

    Add files via upload

---
+++ commands/info/helpdk.js
@@ -0,0 +1,43 @@
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+const { Color, Prefix } = require("../../config.js");
+
+module.exports = {
+  name: "helpdk",
+  aliases: [""],
+  description: "Help Command!",
+  usage: "Help | <Command Name>",
+  run: async(client, message, args) => {
+    
+    message.delete();
+    
+    let embed = new MessageEmbed()
+    .setColor(Color)
+    .setTitle(`${client.user.username} Commands!`)
+    .setDescription(`Use ${Prefix}Help <Command Name> For More Command Information!` + 
+    "\n\n`😉`**Utils**\n`Avatar, Meme, Ascii, Hackid`" + "\n\n" + "`🔐`**Moderation**\n`Clear, Mute, Unmute, Tempmute, Kick, Ban, Unban, Tempban, Warn, Warnings, ResetWarns`" + "\n\n"+
+    "`⌚`**Information**\n`Help, Userinfo, Serverinfo, Ping`")
+    .setFooter(`Requested By ${message.author.username}`)
+    .setTimestamp();
+    
+    if (!args.length) return message.channel.send(embed);
+
+    let cmd =
+      client.commands.get(args[0].toLowerCase()) ||
+      client.commands.get(client.aliases.get(args[0].toLowerCase()));
+
+    let embed2 = new MessageEmbed()
+      .setColor(Color)
+      .setTitle(`${cmd.name} Information!`)
+      .addField(`Aliases`, cmd.aliases || "None!")
+      .addField(`Usage`, cmd.usage || "No Usage")
+      .addField(`Description`, cmd.description || "No Description!")
+      .setTimestamp();
+
+    if (cmd) {
+      return message.channel.send(embed2);
+    } else {
+      return message.channel.send(embed);
+    }
+  }
+};
